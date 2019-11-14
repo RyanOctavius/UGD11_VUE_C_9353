3d5a20ca460f43d5da49ce36b263e2dad68eb7b9
RyanOctavius <ryanoctavius8899@gmail.com>
2019-11-14 13:02:14 +0700

    ugd

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+const DashboardLayout = () => import('../components/dashboardLayout.vue')
+        function loadView(view) {
+            return () => import(`../components/dashboardContents/${view}.vue`)
+        }
+        const routes = [{
+            path: '/',
+            component: DashboardLayout,
+            children: [{
+                name: 'UserController',
+                path: '',
+                component: loadView('userController')
+            },
+            {
+                name: 'BranchesController',
+                path: 'branches',
+                component: loadView('branchesController')
+            }]
+        }, ]
+        Vue.use(Router) 
+            const router = new Router({mode: 'history', routes: routes})
+        export default router
